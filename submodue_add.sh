f11cc2975b33cf425abd4cbd40386ba31e9fbf67
Ufuk Safak <ufuk.safak@additude.se>
2024-03-13 11:21:40 +0100

    hey hey

--- submodue_add.sh
+++ submodue_add.sh
@@ -1,3 +1,4 @@
 git submodule add https://github.com/espressif/kicad-libraries.git
 git submodule add https://github.com/sparkfun/SparkFun-KiCad-Libraries.git
 git submodule add https://github.com/Digi-Key/digikey-kicad-library.git
+git submodule update
